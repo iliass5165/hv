176bba3f72670d9d9f281aaf1f960ae758948f36
iliass <iliass5165@gmail.com>
2016-11-15 09:36:54 +0100

    structure base

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<!-- CSS -->
+	<link rel="stylesheet" href="public/css/materialize.css">
+	<meta charset="UTF-8">
+	<title>HV</title>
+</head>
+<body>
+	
+	
+	
+	<!-- JS -->
+	<script src="public/js/materialize.js"></script>
+</body>
+</html>
