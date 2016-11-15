c336609c79d7fdd9bb0b5724405db9102625be20
iliass <iliass5165@gmail.com>
2016-11-15 16:28:36 +0100

    fix nav

--- index.php
+++ index.php
@@ -13,12 +13,23 @@
 <body>
 	
 	<header>
-		 <nav class=" light-green accent-3 lighten-1" role="navigation">
-		      <ul class="right hide-on-med-and-down">
+		 <nav class=" green accent-3" role="navigation">
+		      <ul class="left hide-on-med-and-down">
+		        <li><a href="index.php">Accueil</a></li>
+		        <li><a class="dropdown-button" href="#!" data-activates="dropdown1">News<i class="material-icons right">arrow_drop_down</i></a></li>
+		        <ul id="dropdown1" class="dropdown-content">
+				  <li><a href="index.php?page=actualite">Actualité</a></li>
+				  <li><a href="index.php?page=evenement">Evenement</a></li>
+				  <li><a href="index.php?page=competition">Competition</a></li>
+				</ul>
 		        <li><a href="#">Navbar Link</a></li>
+	
 		      </ul>
 
 		      <ul id="nav-mobile" class="side-nav">
+		        <li><a href="#">Navbar Link</a></li>
+		        <li><a href="#">Navbar Link</a></li>
+		        <li><a href="#">Navbar Link</a></li>
 		        <li><a href="#">Navbar Link</a></li>
 		      </ul>
 		     <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
@@ -138,6 +149,44 @@
 			<script src="public/js/materialize.js"></script>
 		</div>
 	</div>
+
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+            
 	<script src="public/js/jquery-3.1.1.min.js"></script>
 	<script src="public/js/materialize.min.js"></script>
 	<script>
@@ -145,6 +194,7 @@
 		$( document ).ready(function() {
 		    $('.carousel.carousel-slider').carousel({full_width: true});
 			$('.button-collapse').sideNav();
+			$(".dropdown-button").dropdown();
 		})
 	</script>
 </body>
--- public/page/accueil.php
+++ public/page/accueil.php
@@ -7,6 +7,20 @@
 </div>
         
 
+<div class="col s12 m12">
+  <div class="card blue-grey darken-1">
+    <div class="card-content white-text">
+      <span class="card-title">Dernières Actualités</span>
+      <p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo praesentium blanditiis iure perspiciatis corporis nisi. Voluptates nesciunt doloribus ea, reiciendis commodi nemo quo ratione repudiandae, architecto soluta. Consectetur, quia.
+      <p>
+    </div>
+    <div class="card-action">
+      <a href="#">Lire la suite</a>
+      <a href="#">This is a link</a>
+    </div>
+  </div>
+</div>
 
 <div class="col s6 m6">
   <div class="card blue-grey darken-1">
