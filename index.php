6ca4397769d45bc216b5e1338588018ccde84035
iliass <iliass5165@gmail.com>
2016-11-15 15:36:02 +0100

    nav

--- index.php
+++ index.php
@@ -1,118 +1,152 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<!-- CSS -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+	<!-- CSS + Icons-->
 	<link rel="stylesheet" href="public/css/materialize.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta charset="UTF-8">
 	<title>HV</title>
 </head>
 <body>
 	
+	<header>
+		 <nav class=" light-green accent-3 lighten-1" role="navigation">
+		    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img class="front-page-logo" class="col s3 image-responsive" src="public/images/logo.png"/></a>
+		      <ul class="right hide-on-med-and-down">
+		        <li><a href="#">Navbar Link</a></li>
+		      </ul>
 
-<?php 
-	if (isset($_GET['page']))
-	
-	$page= $_GET['page'];
-	switch ($page) 
-	{
+		      <ul id="nav-mobile" class="side-nav">
+		        <li><a href="#">Navbar Link</a></li>
+		      </ul>
+		     <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+		    </div>
+		  </nav>
+	</header>
+	<div class="container">
+		<div class="row">
+		<?php 
+			if (isset($_GET['page']))
+			{
+				$page= $_GET['page'];
+				switch ($page) 
+				{
 
-			case 'accueil':
-			
+						case 'accueil':
+						
+							include('public/page/accueil.php');
+
+							break;
+						case 'actualite':
+
+							include('public/page/actualite.php');
+
+						break;
+
+						case 'evenement':
+						
+							include('public/page/evenement.php');
+
+						break;
+
+						case 'competition':
+						
+							include('public/page/competition.php');
+
+						break;
+						case 'presentation':
+						
+							include('public/page/presentation.php');
+
+						break;
+
+						case 'association':
+						
+							include('public/page/association.php');
+							
+							break;
+
+							case 'horaire':
+						
+							include('public/page/horaire.php');
+
+							break;
+
+							case 'calendrier':
+						
+							include('public/page/calendrier.php');
+
+							break;
+
+							case 'reglement':
+						
+							include('public/page/reglement.php');
+							break;
+
+							case 'evenement':
+						
+							include('public/page/evenement.php');
+
+							break;
+
+							case 'materiel':
+						
+							include('public/page/materiel.php');
+
+							break;
+
+							case 'technique':
+						
+							include('public/page/technique.php');
+
+							break;
+						
+							case 'tarifInscription':
+						
+							include('public/page/tarifInscription.php');
+
+							break;
+
+							case 'contact':
+						
+							include('public/page/contact.php');
+
+							break;
+
+							case 'photo':
+						
+							include('public/page/photo.php');
+
+							break;
+						
+						
+						default:
+						
+							include('public/page/accueil.php');
+							
+							break;
+					}	
+			}
+			else
+			{
 				include('public/page/accueil.php');
-
-				break;
-			case 'actualite':
-
-				include('public/page/actualite.php');
-
-			break;
-
-			case 'evenement':
-			
-				include('public/page/evenement.php');
-
-			break;
-
-			case 'competition':
-			
-				include('public/page/competition.php');
-
-			break;
-			case 'presentation':
-			
-				include('public/page/presentation.php');
-
-			break;
-
-			case 'association':
-			
-				include('public/page/association.php');
-				
-				break;
-
-				case 'horaire':
-			
-				include('public/page/horaire.php');
-
-				break;
-
-				case 'calendrier':
-			
-				include('public/page/calendrier.php');
-
-				break;
-
-				case 'reglement':
-			
-				include('public/page/reglement.php');
-				break;
-
-				case 'evenement':
-			
-				include('public/page/evenement.php');
-
-				break;
-
-				case 'materiel':
-			
-				include('public/page/materiel.php');
-
-				break;
-
-				case 'technique':
-			
-				include('public/page/technique.php');
-
-				break;
-			
-				case 'tarifInscription':
-			
-				include('public/page/tarifInscription.php');
-
-				break;
-
-				case 'contact':
-			
-				include('public/page/contact.php');
-
-				break;
-
-				case 'photo':
-			
-				include('public/page/photo.php');
-
-				break;
-			
-			
-			default:
-			
-				include('public/page/accueil.php');
-				
-				break;
-		}	
-	?>
-	<!-- JS -->
-	<script src="public/js/materialize.js"></script>
+			}
+			?>
+			<!-- JS -->
+			<script src="public/js/materialize.js"></script>
+		</div>
+	</div>
+	<script src="public/js/jquery-3.1.1.min.js"></script>
+	<script src="public/js/materialize.min.js"></script>
+	<script>
+		
+		$( document ).ready(function() {
+		    $('.carousel.carousel-slider').carousel({full_width: true});
+			$('.button-collapse').sideNav();
+		})
+	</script>
 </body>
 </html>
 
---
+++ public/page/accueil.php
@@ -0,0 +1,13 @@
+<div class="carousel carousel-slider">
+    <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/600/300/food/1"></a>
+    <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/600/300/food/2"></a>
+    <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/600/300/food/3"></a>
+    <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/600/300/food/4"></a>
+</div>
+        
+<div class="col s6">
+	actualite
+</div>
+<div class="col s6">
+	evenement
+</div>
