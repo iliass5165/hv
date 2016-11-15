dacf594c1383f21f0255db29776f1705e6af04fb
ilias <iliass5165@gmail.com>
2016-11-15 21:19:25 +0100

    fix actu

--- public/page/accueil.php
+++ public/page/accueil.php
@@ -10,7 +10,19 @@
 <div class="col s12 m12">
   <div class="card blue-grey darken-1">
     <div class="card-content white-text">
-      <span class="card-title">Dernières Actualités</span>
+      <span class="card-title">Bienvenue</span>
+      <p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo praesentium blanditiis iure perspiciatis corporis nisi. Voluptates nesciunt doloribus ea, reiciendis commodi nemo quo ratione repudiandae, architecto soluta. Consectetur, quia.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eligendi saepe doloremque commodi reiciendis impedit quia modi recusandae! Rerum officia autem atque error nam, nisi itaque molestiae deserunt maxime id.
+      <p>
+    </div>
+  </div>
+</div>
+
+<div class="col s12 m6 l6">
+  <div class="card blue-grey darken-1">
+    <div class="card-content white-text">
+      <span class="card-title">Dernière Actualité</span>
       <p>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo praesentium blanditiis iure perspiciatis corporis nisi. Voluptates nesciunt doloribus ea, reiciendis commodi nemo quo ratione repudiandae, architecto soluta. Consectetur, quia.
       <p>
@@ -22,25 +34,10 @@
   </div>
 </div>
 
-<div class="col s6 m6">
+<div class="col s12 m6 l6">
   <div class="card blue-grey darken-1">
     <div class="card-content white-text">
-      <span class="card-title">Dernières Actualités</span>
-      <p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo praesentium blanditiis iure perspiciatis corporis nisi. Voluptates nesciunt doloribus ea, reiciendis commodi nemo quo ratione repudiandae, architecto soluta. Consectetur, quia.
-      <p>
-    </div>
-    <div class="card-action">
-      <a href="#">Lire la suite</a>
-      <a href="#">This is a link</a>
-    </div>
-  </div>
-</div>
-
-<div class="col s6 m6">
-  <div class="card blue-grey darken-1">
-    <div class="card-content white-text">
-      <span class="card-title">Dernières Actualités</span>
+      <span class="card-title">Dernière évènement</span>
       <p>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo praesentium blanditiis iure perspiciatis corporis nisi. Voluptates nesciunt doloribus ea, reiciendis commodi nemo quo ratione repudiandae, architecto soluta. Consectetur, quia.
       <p>
