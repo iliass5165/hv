83edf7b65fb80ace1c5d264179aa9dd9c221b969
US.Marshals Service <US.Marshals Service>
2016-11-16 14:36:15 +0100

    form contact

---
+++ public/page/contact.php
@@ -0,0 +1,53 @@
+<div class="col s12 m12">
+    <form>
+      <div class="card grey lighten-4 darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Contactez-nous !</span>
+          <div>
+             <div class="row">
+                <div class="input-field col s6">
+                    <input id="first_name" type="text" class="validate">
+                    <label for="first_name">Prénom</label>
+                  </div>
+                <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Nom</label>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="email" type="email" class="validate">
+                <label for="email">Email</label>
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="email" type="text" class="validate">
+                <label for="email">Adresse</label>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="email" type="text" class="validate">
+                <label for="email">Téléphone</label>
+              </div>
+
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">Votre message :</label>
+              </div>
+            </div>
+
+            <button class="btn waves-effect waves-light" type="submit" name="action">Envoyer
+              <i class="material-icons right">send</i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
