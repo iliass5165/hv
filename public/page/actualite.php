d3b0f3e10874c19ba38f9ad968a696b7f5c0a6e3
benjamin <grahling.benjamin@gmail.com>
2016-11-16 14:14:35 +0100

    actualitécervin

--- public/page/actualite.php
+++ public/page/actualite.php
@@ -1,30 +1,37 @@
 <div class="col s12 m12">
-  <div class="card blue-grey darken-1">
+  <div class="card grey lighten-4 darken-1">
     <div class="card-content white-text">
-      <span class="card-title">Card Title</span>
-      <p>I am a very simple card. I am good at containing small bits of information.
-      I am convenient because I require little markup to use effectively.</p>
+      <span class="card-title">News Escalade du Cervin</span>
+      <img class="col s3 image-responsive" src="public/images/cervin.jpg">
+      <p>Notre club va bientôt avoir l'occassion exeptionnel d'escalader la manifique montagne du Cervin, Veillez nous contacter pour plus d'information.
+      </p>
     </div>
     <div class="card-action">
-      <a href="#">This is a link</a>
-      <a href="#">This is a link</a>
+      <a href="index.php?page=showactualite">Lire la suite</a>
+      
+      
     </div>
   </div>
 </div>
 
 <div class="col s12 m12">
-  <div class="card blue-grey darken-1">
+  <div class="card grey lighten-4 darken-1">
     <div class="card-content white-text">
-      <span class="card-title">Card Title</span>
-      <p>I am a very simple card. I am good at containing small bits of information.
-      I am convenient because I require little markup to use effectively.</p>
+      <span class="card-title">Expédition Elbrouz</span>
+      <img class="col s3 image-responsive" src="public/images/elbrouz.jpg">
+      <p>Notre club va bientôt avoir l'occassion exeptionnel d'escalader la manifique montagne du Cervin, Veillez nous contacter pour plus d'information.
+      </p>
     </div>
     <div class="card-action">
-      <a href="#">This is a link</a>
-      <a href="#">This is a link</a>
+      <a href="index.php?page=elbrouz">Lire la suite</a>
+      
     </div>
   </div>
 </div>
+
+
+
+
 
 <div class="col s12 m12">
   <div class="card blue-grey darken-1">
