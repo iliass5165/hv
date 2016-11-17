cb003c43faea8cb1b038223ca7c15b84814dccbc
US.Marshals Service <US.Marshals Service>
2016-11-17 14:59:49 +0100

    bdd_max

--- hv.sql
+++ hv.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Mer 16 Novembre 2016 à 14:42
--- Version du serveur :  5.6.28
--- Version de PHP :  5.6.25
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 17 Novembre 2016 à 14:58
+-- Version du serveur :  10.1.16-MariaDB
+-- Version de PHP :  5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `hv`
@@ -28,17 +34,66 @@ CREATE TABLE `agenda` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `album`
+--
+
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `appartenir2`
+--
+
+CREATE TABLE `appartenir2` (
+  `saison_id` int(11) NOT NULL,
+  `inscription_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `association`
 --
 
 CREATE TABLE `association` (
   `id` int(11) NOT NULL,
-  `nomMembre` varchar(255) NOT NULL,
-  `prenomMembre` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
   `telephone` varchar(10) NOT NULL,
   `mailPerso` varchar(255) NOT NULL,
   `maiPro` varchar(255) NOT NULL,
-  `Reglement` text NOT NULL
+  `Reglement` text NOT NULL,
+  `statut_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `club`
+--
+
+CREATE TABLE `club` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `horaires` varchar(255) NOT NULL,
+  `presentation` text NOT NULL,
+  `photo_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -58,14 +113,114 @@ CREATE TABLE `contact` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `contenir`
+--
+
+CREATE TABLE `contenir` (
+  `photo_id` int(11) NOT NULL,
+  `album_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `detenir`
+--
+
+CREATE TABLE `detenir` (
+  `photo_id` int(11) NOT NULL,
+  `gymnase_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `effectif`
 --
 
 CREATE TABLE `effectif` (
   `id` int(11) NOT NULL,
   `genre` varchar(255) NOT NULL,
-  `categorie` varchar(255) NOT NULL
+  `categorie` varchar(255) NOT NULL,
+  `saison_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gymnase`
+--
+
+CREATE TABLE `gymnase` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `illuster`
+--
+
+CREATE TABLE `illuster` (
+  `photo_id` int(11) NOT NULL,
+  `materiel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscription`
+--
+
+CREATE TABLE `inscription` (
+  `id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `nom` varchar(256) NOT NULL,
+  `prenom` varchar(256) NOT NULL,
+  `message` text NOT NULL,
+  `datenaissance` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `mail` varchar(255) NOT NULL,
+  `licence` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiel`
+--
+
+CREATE TABLE `materiel` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `contenu` text NOT NULL,
+  `categorie_id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo`
+--
+
+CREATE TABLE `photo` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -113,10 +268,37 @@ ALTER TABLE `agenda`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `appartenir2`
+--
+ALTER TABLE `appartenir2`
+  ADD KEY `saison_id` (`saison_id`),
+  ADD KEY `inscription_id` (`inscription_id`);
+
+--
 -- Index pour la table `association`
 --
 ALTER TABLE `association`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `statut_id` (`statut_id`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `club`
+--
+ALTER TABLE `club`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `photo_id` (`photo_id`);
 
 --
 -- Index pour la table `contact`
@@ -125,9 +307,63 @@ ALTER TABLE `contact`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `contenir`
+--
+ALTER TABLE `contenir`
+  ADD KEY `photo_id` (`photo_id`),
+  ADD KEY `album_id` (`album_id`);
+
+--
+-- Index pour la table `detenir`
+--
+ALTER TABLE `detenir`
+  ADD KEY `gymnase_id` (`gymnase_id`),
+  ADD KEY `photo_id` (`photo_id`);
+
+--
 -- Index pour la table `effectif`
 --
 ALTER TABLE `effectif`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `saison_id` (`saison_id`);
+
+--
+-- Index pour la table `gymnase`
+--
+ALTER TABLE `gymnase`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `illuster`
+--
+ALTER TABLE `illuster`
+  ADD KEY `materiel_id` (`materiel_id`),
+  ADD KEY `photo_id` (`photo_id`);
+
+--
+-- Index pour la table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `materiel`
+--
+ALTER TABLE `materiel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorie_id` (`categorie_id`),
+  ADD KEY `photo_id` (`photo_id`);
+
+--
+-- Index pour la table `photo`
+--
+ALTER TABLE `photo`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -158,9 +394,24 @@ ALTER TABLE `utilisateur`
 ALTER TABLE `agenda`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `album`
+--
+ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `association`
 --
 ALTER TABLE `association`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `club`
+--
+ALTER TABLE `club`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `contact`
@@ -171,6 +422,31 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT pour la table `effectif`
 --
 ALTER TABLE `effectif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `gymnase`
+--
+ALTER TABLE `gymnase`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `inscription`
+--
+ALTER TABLE `inscription`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `materiel`
+--
+ALTER TABLE `materiel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `photo`
+--
+ALTER TABLE `photo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `saison`
@@ -187,3 +463,63 @@ ALTER TABLE `statut`
 --
 ALTER TABLE `utilisateur`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `appartenir2`
+--
+ALTER TABLE `appartenir2`
+  ADD CONSTRAINT `inscription_appartenir2_id` FOREIGN KEY (`inscription_id`) REFERENCES `inscription` (`id`),
+  ADD CONSTRAINT `saison_appartenir2_id` FOREIGN KEY (`saison_id`) REFERENCES `saison` (`id`);
+
+--
+-- Contraintes pour la table `association`
+--
+ALTER TABLE `association`
+  ADD CONSTRAINT `statut_association_id` FOREIGN KEY (`statut_id`) REFERENCES `statut` (`id`);
+
+--
+-- Contraintes pour la table `club`
+--
+ALTER TABLE `club`
+  ADD CONSTRAINT `photo_club_id` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`);
+
+--
+-- Contraintes pour la table `contenir`
+--
+ALTER TABLE `contenir`
+  ADD CONSTRAINT `album_contenir_id` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`),
+  ADD CONSTRAINT `photo_contenir_id` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`);
+
+--
+-- Contraintes pour la table `detenir`
+--
+ALTER TABLE `detenir`
+  ADD CONSTRAINT `gymnase_detenir_id` FOREIGN KEY (`gymnase_id`) REFERENCES `gymnase` (`id`),
+  ADD CONSTRAINT `photo_detenir_id` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`);
+
+--
+-- Contraintes pour la table `effectif`
+--
+ALTER TABLE `effectif`
+  ADD CONSTRAINT `saison_effectif_id` FOREIGN KEY (`saison_id`) REFERENCES `saison` (`id`);
+
+--
+-- Contraintes pour la table `illuster`
+--
+ALTER TABLE `illuster`
+  ADD CONSTRAINT `materiel_illustrer_id` FOREIGN KEY (`materiel_id`) REFERENCES `materiel` (`id`),
+  ADD CONSTRAINT `photo_illustrer_id` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`);
+
+--
+-- Contraintes pour la table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `categorie_news_id` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`),
+  ADD CONSTRAINT `photo_news_id` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
