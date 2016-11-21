61b31a485d8624a7bc81323d6d477b916c957864
benjamin <grahling.benjamin@gmail.com>
2016-11-21 10:43:24 +0100

    lulu

---
+++ public/page/materiel&technique.php
@@ -0,0 +1,33 @@
+<div class-"row">
+	<div class="progress">
+	      <div class="indeterminate"></div>
+	 </div>
+</div>
+
+<ul class="collection">
+    
+    <li class="collection-item avatar">
+      <i class="material-icons circle blue">library_books</i>
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle blue">library_books</i>
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle blue">library_books</i>
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+  </ul>
