a2c72308322edb6fadf812e3181c2d7cebfb289a
benjamin <grahling.benjamin@gmail.com>
2016-11-21 08:13:01 +0100

    inscription

--- index.php
+++ index.php
@@ -229,7 +229,9 @@
 		    $('.carousel.carousel-slider').carousel({full_width: true});
 			$('.button-collapse').sideNav();
 			$(".dropdown-button").dropdown();
-		})
+    		$('select').material_select();
+    		$('.datepicker').pickadate({selectMonths: true, selectYears: true, selectYears: 100});
+		});
 	</script>
 </body>
 </html>
---
+++ public/page/tarifInscription.php
@@ -0,0 +1,58 @@
+<form class="col s12">
+  <div class="card grey lighten-4 darken-1">
+    <div class="card-content white-text">
+              <span class="card-title">Inscription</span>
+             
+    <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Nom</label>
+        </div>
+         <div class="input-field col s6">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Prénom</label>
+        </div>
+    </div>
+
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">phone</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Téléphone</label>
+        </div>
+         <div class="input-field col s6">
+          <i class="material-icons prefix">email</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Mail</label>
+        </div>
+     </div>
+
+
+
+      <div class="row">
+        <div class="input-field col s12">
+          <i class="material-icons prefix">mode_edit</i>
+          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+          <label for="icon_prefix2">Commentaire</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s6  offset-s3">
+          <input type="date" class="datepicker">
+          <label>Date de naissance</label>
+        </div>
+      </div>
+
+  
+
+
+
+
+    </div>
+  </div>
+</form>
+
+        
