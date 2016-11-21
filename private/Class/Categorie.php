03ee66bfc621714702d9485ec48cbfd26f319f3c
US.Marshals Service <US.Marshals Service>
2016-11-21 10:59:56 +0100

    ben le moche

---
+++ private/Class/Categorie.php
@@ -0,0 +1,20 @@
+<?php
+
+class Categorie
+{
+	private $id
+	private $titre
+
+}
+
+
+//getter
+public function getId()
+{
+	return $this->id;
+}
+
+public function getTitre()
+{
+	return $this->titre;
+}
---
+++ private/Class/News.php
@@ -0,0 +1,47 @@
+<?php
+
+class News
+{
+	private $id
+	private $titre
+	private $contenu
+	private $categorie
+	private $photo
+}
+
+//getter
+public function getId()
+{
+	return $this->id;
+}
+
+public function getTitre()
+{
+	return $this->titre;
+}
+
+public function getContenu()
+{
+	return $this->contenu;
+}
+
+public function getCategorie()
+{
+	return $this->categorie;
+}
+
+public function getPhoto()
+{
+	return $this->photo;
+}
+
+//setter 
+public function setTitre($titre)
+{
+	$this->titre = $titre;
+}
+
+public function setContenu($contenu)
+{
+	$this->contenu = $contenu;
+}
