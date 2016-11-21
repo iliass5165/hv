4bb962e6acc33347703bd13d0580273b6affa067
iliass <iliass5165@gmail.com>
2016-11-21 13:43:00 +0100

    exemple pdo

---
+++ private/routing.php
@@ -0,0 +1,121 @@
+<?php 
+if (isset($_GET['controller']))
+{
+	$controller= $_GET['controller'];
+	$action= $_GET['action'];
+	switch ($controller) 
+	{
+
+			
+			case 'accueil':
+			
+				include('public/page/accueil.php');
+
+			break;
+
+			case 'actualite':
+
+				include('public/page/actualite.php');
+
+			break;
+
+			case 'evenement':
+			
+				include('public/page/evenement.php');
+
+			break;
+
+			case 'competition':
+			
+				include('public/page/competition.php');
+
+			break;
+			case 'presentation':
+			
+				include('public/page/presentation.php');
+
+			break;
+
+			case 'association':
+			
+			include('public/page/association.php');
+			
+			break;
+
+			case 'horaire':
+		
+			include('public/page/horaire.php');
+
+			break;
+
+			case 'calendrier':
+		
+			include('public/page/calendrier.php');
+
+			break;
+
+			case 'reglement':
+		
+			include('public/page/reglement.php');
+			break;
+
+			case 'evenement':
+		
+			include('public/page/evenement.php');
+
+			break;
+
+		
+			case 'tarifInscription':
+		
+			include('public/page/tarifInscription.php');
+
+			break;
+
+			case 'contact':
+		
+			include('public/page/contact.php');
+
+			break;
+
+			case 'photo':
+		
+			include('public/page/photo.php');
+
+			break;
+			
+			case 'concours':
+		
+			include('public/page/concours.php');
+
+			break;
+
+			case 'compet':
+		
+			include('public/page/compet.php');
+
+			break;
+
+			case 'showactualite';
+
+			include('public/page/showactualite.php');
+
+			break;
+
+			case 'materiel';
+
+			include('public/page/materiel&technique.php');
+
+			break;
+		
+		default:
+		
+			include('public/page/accueil.php');
+			
+			break;
+		}	
+}
+else
+{
+	include('public/page/accueil.php');
+}
---
+++ public/page/materielettechnique.php
@@ -0,0 +1,51 @@
+<div class-"row">
+	<div class="progress">
+	      <div class="indeterminate"></div>
+	 </div>
+</div>
+
+<ul class="collection">
+    
+    <li class="collection-item avatar">
+      <i class="material-icons circle blue">library_books</i>
+      <span class="title">Technique</span>
+      <p>Montage en tête <br>
+         Cette discipline se crée progressivement à la fin du xixe siècle dans les courses des premiers alpinistes vers les grands sommets, avant de se démocratiser au siècle suivant, devenant populaire dès la fin des années 1970. Les premières compétitions officielles sont organisées en 1988 par l'Union internationale des associations d'alpinisme (UIAA). Chaque année est organisée une Coupe du monde de difficulté, de bloc et de vitesse, et tous les deux ans des championnats du monde, l'ensemble étant supervisé par la Fédération internationale d'escalade (IFSC).
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle blue">library_books</i>
+      <span class="title">Technique</span>
+      <p> Descente en rappel</br> 
+      	C'est une technique de descente contrôlée d'un dénivelé vertical (paroi rocheuse, vide), à l'aide d'une corde.
+
+Utilisée principalement en alpinisme, escalade, spéléologie et canyoning, il s'agit aussi d'une technique utilisée dans d'autres contextes pour atteindre depuis le haut des zones difficiles d'accès (secours, maintenance industrielle, construction, inspection...). 
+<a href="https://fr.wikipedia.org/wiki/Descente_en_rappel">Descente en rappel liens</a>
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+  </ul>
+
+	<ul class="collection">
+    
+    <li class="collection-item avatar">
+      <i class="material-icons circle red">library_books</i>
+      <span class="title">Matériel</span>
+      <p>Montage en tête <br>
+         Cette discipline se crée progressivement à la fin du xixe siècle dans les courses des premiers alpinistes vers les grands sommets, avant de se démocratiser au siècle suivant, devenant populaire dès la fin des années 1970. Les premières compétitions officielles sont organisées en 1988 par l'Union internationale des associations d'alpinisme (UIAA). Chaque année est organisée une Coupe du monde de difficulté, de bloc et de vitesse, et tous les deux ans des championnats du monde, l'ensemble étant supervisé par la Fédération internationale d'escalade (IFSC).
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle red">library_books</i>
+      <span class="title">Matériel</span>
+      <p> Descente en rappel</br> 
+      	C'est une technique de descente contrôlée d'un dénivelé vertical (paroi rocheuse, vide), à l'aide d'une corde.
+
+Utilisée principalement en alpinisme, escalade, spéléologie et canyoning, il s'agit aussi d'une technique utilisée dans d'autres contextes pour atteindre depuis le haut des zones difficiles d'accès (secours, maintenance industrielle, construction, inspection...). 
+<a href="https://fr.wikipedia.org/wiki/Descente_en_rappel">Descente en rappel liens</a>
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+    </li>
+  </ul>
