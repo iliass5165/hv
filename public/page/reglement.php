a8e1c468a9453e125963b7306466464af6d9a7a9
US.Marshals Service <US.Marshals Service>
2016-11-21 10:28:18 +0100

    reglement

---
+++ public/page/reglement.php
@@ -0,0 +1,120 @@
+<div class="col s12 m12">
+  <div class="card grey lighten-4 darken-1">
+    <div class="card-content white-text">
+
+      <span class="card-title">Horaires d'entrainement du club.</span>
+      <p style="white-space:pre;">
+Voici quelques informations nécessaires au bon fonctionnement de notre activité "Escalade" destinés aux enfants, aux parents ainsi qu'aux membres adultes:
+
+ 
+
+                * Inscription :- Age minimum requis : 8 ans dans l'année en cours (pour des raisons d'autonomie, donc de sécurité!).
+
+- Un test d'aptitude à l'escalade est réalisé au cours des 2 premières séances afin de valider l'inscription.
+
+                        - Cotisation: plusieurs options au choix pour votre assurance et la revue selon votre catégorie:
+
+                                    - Ecureuil à cadet : 92 € (hors option)
+
+                                    - Junior et senior : 74 € (hors option)
+
+                                    - + 3 € si Option Assurance Formule "Base +".
+                                    Attention : si rien n'est coché concernant les options sur la fiche d'inscription, c'est l'assurance standard qui est retenue
+
+                                    - + 10 € si Option Assurance Formule "Base + +"
+
+                                    - + 5 € si Option Assurance Ski de piste
+
+                                    - + 30 € si Option Assurance VTT
+
+                                    - + 5 € si Option Assurance Slackline et Highline
+
+                                    - + 18 € si Garanties optionnelles indemnités journalières IJ1
+
+                                    - + 30 € si Garanties optionnelles indemnités journalières IJ2
+
+                                    - + 35 € si Garanties optionnelles indemnités journalières IJ3
+
+                                    - + 8 € si vous souhaitez recevoir la revue FFME "Direct Cimes" (4 numéros)
+
+                                    - Rappel : la Formule "Base" étant le minimum concernant l'Assurance, les 10 € lui correspondant sont déjà intégrés au tarif hors option.
+
+                           - Aux familles ayant au moins 3 inscrits au club, nous proposons un tarif préférentiel.
+
+              - Documents à nous retourner :
+
+                            Fiche d'inscription
+                            Certificat médical
+                            Bulletin N°1
+                            Coupon de la 3ème page de ce Règlement
+                            Règlement par chèque
+
+                             - Nous faire parvenir la fiche d'inscription dument complètée et le certificat médical en même temps que votre règlement (chèque libellé
+                             à l'ordre de "Horizon Vertical") ainsi que le bulletin N°1 de la notice d'information assurance au plus tard le 1er octobre 2013 car nous avons
+                             besoin de ces informations pour l'établissement de la licence et la prise en compte de l'assurance. Nous exigeons également le coupon de
+                             la 3ème page de ce Règlement signé par le grimpeur et son responsable légal s'il est mineur. Un coupon par famille est suffisant s'il est
+                             signé par tous les grimpeurs de la famille.
+
+                            - Attention, le certificat médical n'est valable pour les compétitions que si le terme "compétition" est stipulé sur le certificat. Utiliser le
+                            certificat médical type (FFME) qui vous a été fourni.
+
+                            - Cette année encore, licence sera dématérialisée. Cela se traduit par l'envoi de votre licence par e-mail ou par SMS. Il est donc impératif de
+                            renseigner le N° de portable et l'adresse e-mail des parents sur la fiche d'inscription. Vous ne serez pas obligés d'imprimer votre licence mais,
+                            pour les compétitions, à défaut de la présenter, vous devrez simplement justifier de votre identité. Vous recevrez vos codes de connexion
+                            pour accéder à votre Espace licencié sur le site internet FFME (impression de la licence, modification de vos données, communication
+                            interactive, informations diverses, déclaration d'accident, etc.).
+
+                            - N° de portable et adresse e-mail des parents : ces renseignements sur la fiche d'inscription sont également des moyens de communication
+                            qui nous permettent de mieux vous informer et d'échanger diverses informations
+
+     * Sécurité / Discipline : vous n'êtes pas sans savoir que ce sport engendre des risques. Par contre, ces risques sont réduits au strict minimum si les
+     conditions de pratique sont respectées. Les aspects matériel et technique sont pris en charge par les animateurs. La discipline et le comportement sont
+     des notions très importantes quant à la sécurité (et le plaisir de tous). Nous demandons donc, aux parents, d'en faire part aux enfants et de veiller à
+     la bonne compréhension de ce message.
+
+     * Engagement / Motivation : l'escalade, comme toute activité sportive, exige assiduité et motivation aux entrainements, participation active aux compétitions et aux manifestations organisées par le club. Toute absence devra être justifiée par le responsable légal qui préviendra les responsables du club.
+
+     * Compétition : les grimpeurs (écureuil à cadet) doivent participer aux compétitions. Le covoiturage sera privilégié entre parents au départ du gymnase. Un responsable sera présent lors des compétitions dans la mesure du possible. Le coût des inscriptions est pris en charge par le club pour les catégories écureuil à cadet.
+
+     * Sanction : dans le cadre du manquement à une obligation ou de problèmes récurrents de discipline, le bureau sanctionnera le grimpeur; la sanction pouvant aller de la mise à pied temporaire jusqu'au renvoi définitif du club. Cette prise de sanction fera l'objet d'une information en présence du responsable légal
+
+     * Tenue :
+
+        - Tenue de sport obligatoire (un enfant qui n'aura pas une tenue de sport adaptée ne participera pas à l'entraînement).
+
+        - Chaussures:
+
+            - Baskets pour l'échauffement puis chaussons d'escalade pour les exercices.
+
+            - Chaussons d'escalade obligatoires après les vacances de la Toussaint.
+
+             Remarque: nous vous proposons, lors des 2 premières séances d'entraînement après les tests, "une bourse aux chaussures d'escalade" pour procéder à des échanges entre vous. Pour cela, les animateurs se rendent disponibles pour vous conseiller.
+
+    De plus, il est demandé aux enfants de prévoir de quoi se désaltérer pendant les séances.
+
+    Nous précisons que l'utilisation des téléphone sportables ou autres tablettes sont interdits au cours des séances.
+
+                * Prise en charge des enfants : le responsable de l'enfant doit s'assurer qu'au moins un animateur d'escalade est présent pour l'accueillir à l'intérieur du gymnase. Si, au bout de 15 minutes après l'heure normale du cours, aucun animateur n'est présent, la séance est annulée. Le club se dégage de toute responsabilité lors du trajet de l'enfant venant ou partant seul du gymnase. La responsabilité du club n'est engagée que pendant les horaires de l'activité. A la fin de celle-ci, le responsable de l'enfant doit venir le chercher à l'intérieur du gymnase ou sur le lieu de rendez-vous dans le cadre des activités extérieures.
+
+                * Horaires des entraînements : tous les mardis (sauf vacances scolaires) suivant répartition par tranches d'âge:
+
+                       - 1er groupe (les plus petits: écureuils et poussins) :        17h45 à 19h15
+
+                       - 2ème groupe (les plus grands: à partir de benjamin) :     19h00 à 21h00
+
+Tous les samedis (sauf vacances scolaires), de 14 à 16h, à partir de la catégorie minime pour les grimpeurs autonomes, séance libre à la salle de bloc uniquement, sous la surveillance d'un responsable du club. Des séances exceptionnelles pourront être organisees par le club en week-end ou pendant les vacances dont les horaires seront precises au cas par cas.
+
+                *  Fin de séances : chaque grimpeur doit participer au rangement du matériel. Le débriefing (prises de consigne) est obligatoire en fin de séance.
+
+                * Gymnase : pour préserver les revêtements de sol spécifiques dans les différentes salles, nous demandons aux parents (et frères et soeurs) de rester uniquement sur la partie carrelée du gymnase.
+
+                * Tee-shirt du club : comme chaque année, nous avons proposerons un tee-shirt au logo du club.
+
+                *  Statuts du club : les statuts sont à votre disposition. Faire la demande auprès d'un responsable du club pour lecture
+
+En savoir plus : http://horizon-vertical-damparis-tava1.webnode.fr/r%c3%a8glement/
+
+    </div>
+
+ </div>
+</div>
