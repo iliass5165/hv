c2aa9f7cfb108c418b4deb5a782dff34ad46c0c8
Administrator <admin@example.com>
2016-12-12 13:24:07 +0100

    max pageCOnnexion

--- private/Controller/DefaultController.php
+++ private/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 switch($action)
 {
-	
+	case 'login':
+		include('public/page/connexion/connexion.php');
+	break;
 	default:
 		include('public/page/accueil.php');
 	break;
---
+++ public/page/connexion/connexion.php
@@ -0,0 +1,20 @@
+ <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+          <label for="first_name">Votre identifiant </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      
+      <a class="waves-effect waves-light btn">button</a>
+
+    </form>
+  </div>
